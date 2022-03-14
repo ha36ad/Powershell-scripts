7548a595da8a9c5603422590e4c84d0919931d2e
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2022-03-14 06:20:04 -0400

    Create read_file_and_download_csv.ps1
    Script reads a file's contents with valid urls to csv files, then stores them in a temp downloads folder, located in the C drive. It has a logging folder to record exceptions occured.

---
+++ read_file_and_download_csv.ps1
@@ -0,0 +1,46 @@
+# Check number of args
+If ($args.Count -ne 1) {
+    Write-Host "Please supply a file path."
+    Exit
+}
+
+$SourcePath = $args[0]
+
+# Check if the path is valid
+If (-not (Test-Path $SourcePath -PathType leaf)) {
+    Write-Host "Please enter a valid file path."
+    Exit
+}
+
+# Get a list of urls
+$files = Get-Content -Path $SourcePath
+
+$tempdir = "C:\TestDownloads"
+
+# Create a directory to store the downloaded files, if it doesn't exist
+if (-not(Test-Path -Path $tempdir)) {
+    New-Item -ItemType Directory -Path $tempdir
+}
+
+# Send a download request to each of the contents in the file
+foreach ($file in $files) {
+    try {
+        $tempname = $file.name
+        $tempfile = "$tempdir\$tempname.csv"
+        New-Item -ItemType Directory -Path $tempfile
+        Invoke-WebRequest $file -OutFile $tempfile -ErrorAction Stop
+    }
+    catch {
+        # Create a log path if it doesnt exist
+        $LogPath = "C:\Logs"
+        $LogFile = "$LogPath\log_session.txt"
+        If (-not (Test-Path -Path $LogPath)) {
+            Write-Host "no log folder found, creating one named $SourcePath"
+            New-Item -ItemType Directory -Path $LogPath -ErrorAction Stop
+            New-Item -ItemType File -Path $LogFile -ErrorAction Stop      
+        }
+
+        $_.Exception | Out-File $LogFile -Append
+        continue
+    }
+}
