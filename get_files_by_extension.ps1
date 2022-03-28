a54f7119c82e6dd6c3c097cb630ed41adef50109
Hammad Khan <59400114+hailaider@users.noreply.github.com>
2022-03-28 11:42:55 -0400

    Create get_files_by_extension.ps1
    Added a script that takes in a folder path and an extension name and returns a list of files that match the extension.

---
+++ get_files_by_extension.ps1
@@ -0,0 +1,14 @@
+# Check number of args
+If ($args.Count -lt 2) {
+    Write-Host "Please supply a folder path and a file extension."
+    Exit
+}
+$SourcePath = $args[0]
+$Extension = "*" + $args[1]
+#Check if the path is valid
+If (-not(Test-Path $SourcePath)) {
+    Write-Host "Please enter a valid Path."
+    Exit
+}
+
+Get-ChildItem -Path $SourcePath -Filter $Extension -Recurse
